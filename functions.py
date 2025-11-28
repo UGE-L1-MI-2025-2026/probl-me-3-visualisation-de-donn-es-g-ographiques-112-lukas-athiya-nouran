85e643a35ae1925421b7744b21214fc25838d839
NOURAN RETIMA <nouran.retima@nomachine.univ-eiffel.fr>
2025-11-28 12:48:13 +0100

    carte

--- functions.py
+++ functions.py
@@ -1,15 +1,8 @@
 import math as m
 import fltk
 import shapefile
+from tkinter import filedialog as fd
 
-H = 600
-L = 800
-
-sf = shapefile.Reader("departements-20180101.shp") #ouverture du fichier shapefile
-
-fltk.cree_fenetre(L, H)
-
-centre = 0
 
 def conv_rad_degr(rad):
     return (rad*180)/m.pi
@@ -20,22 +13,54 @@ def conv_degr_rad(degre):
 def fonct_mercator(latitude):
     return  m.log(m.tan((latitude / 2) + (m.pi / 4)))
 
-        
+def france():
+    H = 1200
+    L = 1600
+    HH = 600
+    LL = 800
+    ech = 4
+    echh = 3
+    sf = shapefile.Reader("departements-20180101.shp") #ouverture du fichier shapefile
+    fltk.cree_fenetre(L, H)
+    centre = 0
+    for i in range(101):
+        nouvelle_coordo = []
+        for coord in sf.shape(i).points:
+            longitude, latitude = conv_degr_rad(coord[0]) , conv_degr_rad(coord[1])
+            merc = fonct_mercator(latitude)
+            x = (L/2) * (longitude - centre)*echh + 800
+            y = H - (H/2) * merc*ech + 1600
+            nouvelle_coordo.append((x,y))
+        fltk.polygone(nouvelle_coordo, epaisseur = 1)
+        print(i)
+    fltk.mise_a_jour()
+    fltk.attend_ev()
+    fltk.ferme_fenetre()
 
-for i in range(101):
-    nouvelle_coordo = []
-    for coord in sf.shape(i).points:
-        longitude, latitude = conv_degr_rad(coord[0]) , conv_degr_rad(coord[1])
-        
-        x = (L/2) * (longitude - centre)       
-        y = (H/2) * fonct_mercator(latitude)
-        nouvelle_coordo.append((x,y))
-    fltk.polygone(nouvelle_coordo, epaisseur = 2)
-    print(i)
+def dessiner(lezip):
+    H = 1200
+    L = 1600
+    HH = 600
+    LL = 800
+    ech = 4
+    echh = 3
+    sf = shapefile.Reader(lezip) #ouverture du fichier shapefile
+    fltk.cree_fenetre(L, H)
+    centre = 0
+    for i in range(101):
+        nouvelle_coordo = []
+        for coord in sf.shape(i).points:
+            longitude, latitude = conv_degr_rad(coord[0]) , conv_degr_rad(coord[1])
+            merc = fonct_mercator(latitude)
+            x = (L/2) * (longitude - centre)*echh + 800
+            y = H - (H/2) * merc*ech + 1600
+            nouvelle_coordo.append((x,y))
+        fltk.polygone(nouvelle_coordo, epaisseur = 1)
+        print(i)
+    fltk.mise_a_jour()
+    fltk.attend_ev()
+    fltk.ferme_fenetre()
+filename = fd.askopenfilename()
+dessiner(f"{filename}/land-polygons-complete-4326/land_polygons.shp")
 
-
-
-fltk.mise_a_jour()
-fltk.attend_ev()
-fltk.ferme_fenetre()
 
