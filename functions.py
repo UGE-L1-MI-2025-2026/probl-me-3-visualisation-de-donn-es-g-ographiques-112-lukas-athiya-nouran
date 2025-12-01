080cbccb04273ae33e917effdf8880f96afd20c4
Tulyboy <leogane.lukas@gmail.com>
2025-12-01 12:47:05 +0100

    interaction + deplacement de variable

--- functions.py
+++ functions.py
@@ -13,16 +13,11 @@ def conv_degr_rad(degre):
 def fonct_mercator(latitude):
     return  m.log(m.tan((latitude / 2) + (m.pi / 4)))
 
-def france():
-    H = 1200
-    L = 1600
-    HH = 600
-    LL = 800
+def france(L, H, sf):
     ech = 4
     echh = 3
-    sf = shapefile.Reader("departements-20180101.shp") #ouverture du fichier shapefile
-    fltk.cree_fenetre(L, H)
     centre = 0
+    total = []
     for i in range(101):
         nouvelle_coordo = []
         for coord in sf.shape(i).points:
@@ -31,11 +26,13 @@ def france():
             x = (L/2) * (longitude - centre)*echh + 800
             y = H - (H/2) * merc*ech + 1600
             nouvelle_coordo.append((x,y))
-        fltk.polygone(nouvelle_coordo, epaisseur = 1)
-        print(i)
-    fltk.mise_a_jour()
-    fltk.attend_ev()
-    fltk.ferme_fenetre()
+        print(fltk.polygone(nouvelle_coordo, epaisseur = 1))
+
+        total.append(nouvelle_coordo)
+    print("Fini")
+    return total
+
+
 
 def dessiner(lezip):
     H = 1200
@@ -60,7 +57,6 @@ def dessiner(lezip):
     fltk.mise_a_jour()
     fltk.attend_ev()
     fltk.ferme_fenetre()
-filename = fd.askopenfilename()
-dessiner(f"{filename}/land-polygons-complete-4326/land_polygons.shp")
-
+#filename = fd.askopenfilename()
+#dessiner(f"{filename}/land-polygons-complete-4326/land_polygons.shp")
 
--- interaction.py
+++ interaction.py
@@ -1,33 +1,37 @@
-import fltk as f
+import fltk
 import shapefile
-
+import functions
 # polygone -> survole -> indice -> departement dans le shapefilee -> info du département a cette indice
 
 MARGE = 2
-
-f.cree_fenetre(800, 600)
-
-
+H = 1200
+L = 1600
 
 sf = shapefile.Reader("data/departement_shapefile/departements-20180101.shp")
+fltk.cree_fenetre(L, H)
+liste_points = functions.france(L, H, sf)
 
-def rectangle(x1, y1):
+dico = {f'{i}': points for i,points in enumerate(liste_points)}
+precedent = None
+
+
+def rectangle(x1, y1, tag):
     """
     Affiche un rectangle 
     """
     x2 = x1 + 80
     y2 = y1 + 20
-    f.rectangle(x1, y1, x2, y2, epaisseur = 1, remplissage = "white")
+    fltk.rectangle(x1, y1, x2, y2, epaisseur = 1, remplissage = "white", tag = tag)
 
 
-def texte(x1, y1, marge, info: list[str]):
+def texte(x1, y1, marge, info: list[str], tag):
     """
     Affiche le nom de la commune dans le rectangle
     """
     nom = info[0]
     x1 += marge
     y1 += marge + 2
-    f.texte(x1,y1, chaine = nom, ancrage = "nw", taille = 8)
+    fltk.texte(x1,y1, chaine = nom, ancrage = "nw", taille = 8, tag = tag)
 
 
 def milieu(points: list):
@@ -57,22 +61,60 @@ def milieu(points: list):
 
 
 
-for i in range(101):
-    points = [((20 * coord[0] + 300, 20 * coord[1] - (40*20) + 200)) for coord in sf.shape(i).points]
-    x, y = milieu(points)
-    rectangle(x,y)  
-    texte(x, y, 2 ,[sf.record(i)[1]])
+
+x2 = L - 10
+y2 = H - 10
+x1 = x2 - 80
+y1 = y2 - 20
+
+fltk.rectangle(x1, y1, x2, y2, remplissage = "white", tag = "avancer")
+fltk.texte(x1 + 3, y1 - 3, chaine = "avancer", ancrage = "nw", taille = 15)
+
+x2 = 90
+y2 = H-10
+x1 = 10
+y1 = y2 -20
+fltk.rectangle(x1, y1, x2, y2, remplissage = "white", tag = "reculer")
+fltk.texte(x1 +3, y1 - 2, chaine = "reculer", ancrage = "nw", taille = 15)
 
 
+def reculer():
+    print('reculer')
+
+def avancer():
+    print('avancer')
 
 
 while True:
-    ev = f.donne_ev()
-    if f.type_ev(ev) == "Quitte":
-        f.efface_tout()
-        f.ferme_fenetre()
-    
-    if f.objet_survole():
-        texte = sf.record(f.objet_survole())
 
-    f.mise_a_jour()
+    ev = fltk.donne_ev()
+
+    if fltk.objet_survole():
+        if type(fltk.objet_survole()) == int:
+            if precedent is not None:
+                fltk.efface("1")
+            departement = fltk.objet_survole()
+            print(departement)
+            precedent = departement
+            
+            if departement is not None:
+                x, y = milieu(dico[f"{departement-1}"])
+                rectangle(x,y, tag = "1")
+                texte(x, y, 2 ,[sf.record(departement)[1]], tag = "1")
+            
+            
+        else:
+            if fltk.type_ev(ev) == "ClicGauche":
+                if fltk.est_objet_survole("reculer"):
+                    reculer()
+                    
+                elif fltk.est_objet_survole("avancer"):
+                    avancer()
+
+        fltk.efface(precedent)
+    elif fltk.type_ev(ev) == "Quitte":
+        print("non")
+        fltk.efface_tout()
+        fltk.ferme_fenetre()
+    fltk.mise_a_jour()
+
